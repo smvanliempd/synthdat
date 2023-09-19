fc1079fb02f5b6a742092b534758a4922ff9cde6
smvanliempd <smvanliempd@gmail.com>
2023-09-19 11:12:44 +0200

    added models

--- analysis.R
+++ analysis.R
@@ -16,6 +16,7 @@ ds <- syntdat(N_groups   = 3,
 plot_dat(ds)
 
 # model data
+# random-effects model
 f00 <- brmsformula(Value ~ Treatment + (1|Sample))
 (pri00 <- get_prior(f00, data = ds))
 pri00$prior[1:3] <- "normal(0,1)"
@@ -27,26 +28,32 @@ fit00 <- brm(f00, data = ds,
              cores = 4,
              iter = 5000)
 fit00
-prior_summary(fit00)
-posterior_summary(fit00)
 
-# fit00.1 <- update(fit00,
-#               newdata = ds,
-#               chains = 4,
-#               cores = 4,
-#               iter = 5000)
-# fit00.1
-
-f01 <- brmsformula( Value ~ Treatment + (1|gr(Sample, by = Treatment)))
-(pri01 <- get_prior(f01, data = ds))
-pri01$prior[1:3] <- "normal(0,1)"
-pri01$prior[4] <- "normal(1,1)"
-pri01$prior[5:8] <- "exponential(1)"
-fit01 <- brm(f01, data = ds,
-             prior = pri01,
+# random-effects model, allowing for varying sigmas for repeated measures in treatment groups.
+# in order to get sigmas for replicates in groups: exp(sigma_Intercept) * exp(sigma_TreatmentX)
+f02 <- brmsformula( Value ~ Treatment + (1|Sample),
+                    sigma ~ Treatment)
+(pri02 <- get_prior(f02, data = ds))
+pri02$prior[2:3] <- "normal(0,0.5)"
+pri02$prior[4]   <- "normal(1,0.5)"
+pri02$prior[5]   <- "exponential(1)"
+pri02$prior[9:11] <- "student_t(3, 0, 2.5)"
+(pri02 <- pri02[ pri02$prior != "", ])
+fit02 <- brm(f02, data = ds,
+             prior = pri02,
              chains = 4,
              cores = 4,
-             iter = 5000)
-fit01
-prior_summary(fit01)
-posterior_summary(fit01)
+             iter = 5000,
+             control = list(adapt_delta = 0.9))
+fit02
+fit02.1 <- update(fit02,
+                  chains = 4,
+                  cores = 4,
+                  iter = 5000,
+                  control = list(adapt_delta = 0.99))
+fit02.1
+
+
+
+
+
